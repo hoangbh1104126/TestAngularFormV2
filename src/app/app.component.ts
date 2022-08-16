f02221c657ad02ced90a98f4c3762b68527bdb61
hoangbh1104126 <110478946+hoangbh1104126@users.noreply.github.com>
2022-08-16 23:09:26 +0700

    Add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, QueryList, ViewChildren } from '@angular/core';
+import { TestformComponent } from './testform/testform.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular100days';
-  name = "Admin";
-  num = 1;
-  progress = 50;
-  checked = true;
-  userName = "Test";
+export class AppComponent implements AfterViewInit {
+  @ViewChildren(TestformComponent) testForm !: QueryList<any>;
+
+  ngAfterViewInit(): void {
+    //throw new Error('Method not implemented.');
+    this.testForm.forEach(testForm => console.log(testForm));
+  }
+
+  title : string = 'angular100days';
+  name : string = "Admin";
+  num : number = 1;
 
   GetPass(password : string): void{
     alert("Your password is: " + password);
@@ -33,5 +38,5 @@ export class AppComponent {
       console.log("Remove note success!");
     }
     this.num--;
-}
+  }
 }
